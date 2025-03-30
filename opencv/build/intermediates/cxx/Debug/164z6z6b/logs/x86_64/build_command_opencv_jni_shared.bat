387cb3b1e232944ceafe250e2d3a4217fa8361e7
mppjuro <mppjuro@gmail.com>
2025-03-30 23:16:05 +0200

    przejście z kapt na ksp, dodano opencv

---
+++ opencv/build/intermediates/cxx/Debug/164z6z6b/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\PJURO\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\PJURO\\Desktop\\FIPscan\\opencv\\.cxx\\Debug\\164z6z6b\\x86_64" ^
+  opencv_jni_shared
