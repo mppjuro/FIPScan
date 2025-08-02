38c33d866497d3cdb7b2083f420a58cee3dca269
mppjuro <mppjuro@gmail.com>
2025-08-03 00:35:13 +0200

    fixed file DiagnosisFragment.kt

---
+++ opencv/build/intermediates/cxx/Debug/543s4n3f/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\PJURO\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\PJURO\\Desktop\\FIPScan\\opencv\\.cxx\\Debug\\543s4n3f\\x86_64" ^
+  opencv_jni_shared
