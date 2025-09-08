104b831af11febb5b7cd2187852309cd4de332e5
mppjuro <mppjuro@gmail.com>
2025-09-08 15:29:35 +0200

    minor improvements

---
+++ opencv/build/intermediates/cxx/Debug/4c5814d2/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\PJURO\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\PJURO\\Desktop\\FIPScan\\opencv\\.cxx\\Debug\\4c5814d2\\x86_64" ^
+  opencv_jni_shared
