e87f2abcc3eb96ac7576d7696bc0f8031bfb14cf
mppjuro <mppjuro@gmail.com>
2025-09-22 19:19:32 +0200

    dependencies versions updated, minor UI updates
    Former-commit-id: bdb039b49a76c865deb4532434e9125d58c9aa6a

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
-val targetCompatibility by extra(JavaVersion.VERSION_17)
+
+val targetCompatibility by extra(JavaVersion.VERSION_23)
+
 buildscript {
     repositories {
         google()
     }
     dependencies {
-        val nav_version = "2.8.9"
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.4")
     }
 }
