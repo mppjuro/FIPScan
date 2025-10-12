83f6d973e2bff8d1e0ee4826a599539867b74d61
mppjuro <mppjuro@gmail.com>
2025-10-12 21:38:59 +0200

    added bottom margin to pdf reports

--- opencv/build/intermediates/cxx/RelWithDebInfo/3m6t3r5a/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\PJURO\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\PJURO\\Desktop\\FIPScan\\opencv\\.cxx\\RelWithDebInfo\\3m6t3r5a\\armeabi-v7a" ^
-  opencv_jni_shared
